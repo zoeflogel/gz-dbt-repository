8334a1956344b8df393b09a5e236e3e81144a094
zflogel@gmail.com <zflogel@gmail.com>
2025-07-31 10:47:43 +0000

    commit challenge 2 day 2 dbt

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,3 +1,5 @@
+
+WITH orders_per_day AS(
 SELECT
     date_date,
     COUNT(DISTINCT orders_id) AS nb_transactions,
@@ -11,3 +13,17 @@ SELECT
     SUM(quantity) AS quantity
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
+)
+SELECT
+    date_date,
+    revenue,
+    margin,
+    operational_margin,
+    purchase_cost,
+    shipping_fee,
+    log_cost,
+    ship_cost,
+    quantity,
+    ROUND(revenue/NULLIF(nb_transactions,0),2) AS average_basket
+FROM orders_per_day
+ORDER BY date_date DESC
