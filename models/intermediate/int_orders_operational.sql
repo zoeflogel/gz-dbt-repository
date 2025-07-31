843f417aa99fd34a1e81c3e2e71cc0c0174c3d81
zflogel@gmail.com <zflogel@gmail.com>
2025-07-31 09:36:59 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,15 +1,15 @@
 SELECT
     orders.orders_id,
     orders.date_date,
-    ROUND(orders.margin + sales.shipping_fee - (ship.log_cost + ship.ship_cost),2) AS operational_margin,
+    ROUND(orders.margin + ship.shipping_fee - (ship.logcost + ship.ship_cost),2) AS operational_margin,
     orders.quantity,
     orders.revenue,
     orders.purchase_cost,
     orders.margin,
     ship.shipping_fee,
-    ship.log_cost,
+    ship.logcost,
     ship.ship_cost
 FROM {{ref("int_orders_margin")}} orders
-LEFT JOIN {{ref("stg_gz_source__ship")}} sales
+LEFT JOIN {{ref("stg_gz_source__ship")}} ship
     USING(orders_id)
 ORDER BY orders_id desc
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,13 @@
+SELECT
+    date_date,
+    COUNT(DISTINCT orders_id) AS nb_transactions,
+    ROUND(SUM(revenue),0) AS revenue,
+    ROUND(SUM(margin),0) AS margin,
+    ROUND(SUM(operational_margin),0) AS operational_margin,
+    ROUND(SUM(purchase_cost),0) AS purchase_cost,
+    ROUND(SUM(shipping_fee),0) AS shipping_fee,
+    ROUND(SUM(logcost),0) AS log_cost,
+    ROUND(SUM(ship_cost),0) AS ship_cost,
+    SUM(quantity) AS quantity
+FROM {{ref("int_orders_operational")}}
+GROUP BY date_date
